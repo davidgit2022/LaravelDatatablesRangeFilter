ebbab6cd50be85342f41f8613b8908bbd0814bf0
Deivy Potosi <deibypotosi535@gmail.com>
2023-06-10 09:23:29 -0500

    Finalizado

---
+++ resources/views/students.blade.php
@@ -0,0 +1,165 @@
+//resources/views/students.blade.php
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 9 Datatables Date Range Filter</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Laravel 9 Datatables Date Range Filter</h1>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text btn btn-primary text-white" id="basic-addon1"><i
+                                        class="fas fa-calendar-alt"></i></span>
+                            </div>
+                            <input type="text" class="form-control" id="start_date" placeholder="Start Date"
+                                readonly>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text btn btn-primary text-white" id="basic-addon1"><i
+                                        class="fas fa-calendar-alt"></i></span>
+                            </div>
+                            <input type="text" class="form-control" id="end_date" placeholder="End Date" readonly>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button id="filter" class="btn btn-primary">Filter</button>
+                    <button id="reset" class="btn btn-warning">Reset</button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="display" id="records" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Standard</th>
+                                <th>Percentage</th>
+                                <th>Result</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+    <!-- Datepicker -->
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <!-- Datatables -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <!-- Momentjs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script>
+        $(function() {
+            $("#start_date").datepicker({
+                "dateFormat": "yy-mm-dd"
+            });
+            $("#end_date").datepicker({
+                "dateFormat": "yy-mm-dd"
+            });
+        });
+        // Fetch records
+        function fetch(start_date, end_date) {
+            $.ajax({
+                url: "{{ route('students/records') }}",
+                type: "GEt",
+                data: {
+                    start_date: start_date,
+                    end_date: end_date
+                },
+                dataType: "json",
+                success: function(data) {
+                    // Datatables
+                    let i = 1;
+                    $('#records').DataTable({
+                        "data": data.students,
+                        // responsive
+                        "responsive": true,
+                        "columns": [{
+                                "data": "id",
+                                "render": function(data, type, row, meta) {
+                                    return i++;
+                                }
+                            },
+                            {
+                                "data": "name"
+                            },
+                            {
+                                "data": "standard",
+                                "render": function(data, type, row, meta) {
+                                    return `${row.standard}th Standard`;
+                                }
+                            },
+                            {
+                                "data": "percentage",
+                                "render": function(data, type, row, meta) {
+                                    return `${row.percentage}%`;
+                                }
+                            },
+                            {
+                                "data": "result"
+                            },
+                            {
+                                "data": "created_at",
+                                "render": function(data, type, row, meta) {
+                                    return moment(row.created_at).format('DD-MM-YYYY');
+                                }
+                            }
+                        ]
+                    });
+                }
+            });
+        }
+        fetch();
+        // Filter
+        $(document).on("click", "#filter", function(e) {
+            e.preventDefault();
+            var start_date = $("#start_date").val();
+            var end_date = $("#end_date").val();
+            if (start_date == "" || end_date == "") {
+                alert("Both date required");
+            } else {
+                $('#records').DataTable().destroy();
+                fetch(start_date, end_date);
+            }
+        });
+        // Reset
+        $(document).on("click", "#reset", function(e) {
+            e.preventDefault();
+            $("#start_date").val(''); // empty value
+            $("#end_date").val('');
+            $('#records').DataTable().destroy();
+            fetch();
+        });
+    </script>
+</body>
+
+</html>
